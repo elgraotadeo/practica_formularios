997fcbcf6c44a25b5f41c092c3e9ddb808937ea8
elgraotadeo <119655217+elgraotadeo@users.noreply.github.com>
2023-02-03 09:53:01 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function change_doc(){
+    var back_holder = document.getElementById('num_doc');
+    back_holder.setAttribute("placeholder","Número de pasaporte");
+}
+
+function change_doc_back(){
+    var back_holder = document.getElementById('num_doc');
+    back_holder.setAttribute("placeholder","DNI o Tarjeta de residencia");
+}
